7c6c8d1c575cdf448ac18ae5bc47e5fed3d63bba
femsworld <femsworld@gmail.com>
2024-05-18 02:55:13 +0300

    Display the car images in car page and car details pages

--- frontend/src/main.go
+++ frontend/src/main.go
@@ -7,12 +7,20 @@ import (
 )
 
 func main() {
+	// Serve static files for frontend
+	http.Handle("/src/static/", http.StripPrefix("/src/static/", http.FileServer(http.Dir("frontend/src/static"))))
+
+	// Serve static files for images
+	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/home/femsworld/cars/api/src/img"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../cars/api/src/img"))))
+
+	// Handle other routes
 	http.HandleFunc("/", functions.HomeHandler)
 	http.HandleFunc("/cars", functions.CarHandler)
 	http.HandleFunc("/cars/", functions.CarDetailHandler)
 	http.HandleFunc("/filters", functions.AdvancedFiltersHandler)
 	http.HandleFunc("/compare", functions.ComparisonsHandler)
-	http.Handle("/src/static/", http.StripPrefix("/src/static/", http.FileServer(http.Dir("frontend/src/static"))))
+
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
